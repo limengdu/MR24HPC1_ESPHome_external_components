93a51480ac49d57b1cabe55315862b418d2b9642
limengdu <37475446+limengdu@users.noreply.github.com>
2023-11-14 13:55:32 +0800

    Fix

--- components/mr24hpc1/sensor.py
+++ components/mr24hpc1/sensor.py
@@ -3,19 +3,24 @@ from esphome.components import sensor
 import esphome.config_validation as cv
 from esphome.const import (
     DEVICE_CLASS_DISTANCE,
+    UNIT_METER,
 )
 from . import CONF_MR24HPC1_ID, mr24hpc1Component
 
-CONF_CUSTOMPRESENCEOFDETECTION = 'custompresenceofdetection'
-
 AUTO_LOAD = ["mr24hpc1"]
 
-CONFIG_SCHEMA = {
-    cv.GenerateID(CONF_MR24HPC1_ID): cv.use_id(mr24hpc1Component),
-    cv.Optional(CONF_CUSTOMPRESENCEOFDETECTION): sensor.sensor_schema(
-        device_class=DEVICE_CLASS_DISTANCE, icon="mdi:signal-distance-variant"
-    ),
-}
+CONF_CUSTOMPRESENCEOFDETECTION = 'custompresenceofdetection'
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_MR24HPC1_ID): cv.use_id(mr24hpc1Component),
+        cv.Optional(CONF_CUSTOMPRESENCEOFDETECTION): sensor.sensor_schema(
+            device_class=DEVICE_CLASS_DISTANCE,
+            unit_of_measurement=UNIT_METER,
+            icon="mdi:signal-distance-variant",
+        ),
+    }
+)
 
 async def to_code(config):
     mr24hpc1_component = await cg.get_variable(config[CONF_MR24HPC1_ID])
